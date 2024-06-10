4e00fa7c1563ab5b66e83a8ee397627b9521ab34
kuraqura88 <167050509+kuraqura88@users.noreply.github.com>
2024-06-10 19:46:43 +0900

    Feat.Light
    이동훈
    
    빛 추가
    
    fix.ToggleCursor
    커서 버그 픽스

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -48,30 +48,13 @@ public class GameManager : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            Time.timeScale = 1f;
-        }
-    }
 
-    public void ToggleCursor(bool active)
-    {
-        isToggle = active;
-        if (isToggle)
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            Time.timeScale = 0f;
         }
         else
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            Time.timeScale = 1f;
+
         }
     }
 }
