7a221b434b2226899a37e163c5a1b06fb09441d6
kuraqura88 <167050509+kuraqura88@users.noreply.github.com>
2024-06-10 21:31:06 +0900

    Fix.DiaryText
    이동훈
    
    광원 삭제
    다이어리 텍스트 수정

--- Assets/Scripts/_P_u__zzle/LightController.cs
+++ Assets/Scripts/_P_u__zzle/LightController.cs
@@ -1,23 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-    public Light lightSource;
+    public Light[] lights; // 전구들
+    public int[] correctOrder; // 올바른 순서
+    private int currentStep = 0; // 현재 단계
 
-    public void TurnOn()
+    public void SwitchPressed(int switchId)
     {
-        lightSource.enabled = true;
+        // currentStep이 correctOrder의 범위를 벗어나지 않도록 확인합니다.
+        if (currentStep < correctOrder.Length && correctOrder[currentStep] == switchId)
+        {
+            currentStep++;
+            UpdateLights(true);
+
+            // 정답을 모두 맞춘 경우 currentStep을 리셋합니다.
+            if (currentStep == correctOrder.Length)
+            {
+                Debug.Log("Puzzle Solved!");
+                currentStep = 0;
+            }
+        }
+        else
+        {
+            currentStep = 0;
+            UpdateLights(false);
+        }
     }
 
-    public void TurnOff()
+    private void UpdateLights(bool correct)
     {
-        lightSource.enabled = false;
-    }
-
-    public bool IsOn()
-    {
-        return lightSource.enabled;
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (correct)
+            {
+                if (i < currentStep)
+                {
+                    lights[i].enabled = true;
+                }
+                else
+                {
+                    lights[i].enabled = false;
+                }
+            }
+            else
+            {
+                lights[i].enabled = false;
+            }
+        }
     }
 }
--- Assets/Scripts/_P_u__zzle/LightManager.cs
+++ Assets/Scripts/_P_u__zzle/LightManager.cs
@@ -5,63 +5,10 @@ using UnityEngine.UI;
 using TMPro;
 public class LightManager : MonoBehaviour
 {
-    public LightController[] lights;
-    public Button[] buttons;
-    public TMP_Text resultText;
-    public int[] correctSequence = { 0, 1, 2 }; // 버튼을 눌러야 하는 올바른 순서
-    private int currentStep = 0;
-
-    private void Start()
+    public int switchId; // 버튼 ID
+    public LightController lightController; // 전구 컨트롤러
+    public void OnButtonPressed()
     {
-        ResetLights();
-        foreach (Button button in buttons)
-        {
-            button.onClick.AddListener(() => OnButtonClick(button));
-        }
-        resultText.text = "";
-    }
-
-    private void ResetLights()
-    {
-        foreach (LightController light in lights)
-        {
-            light.TurnOff();
-        }
-        currentStep = 0;
-    }
-
-    public void OnButtonClick(Button button)
-    {
-        int buttonIndex = System.Array.IndexOf(buttons, button);
-        if (buttonIndex == correctSequence[currentStep])
-        {
-            lights[buttonIndex].TurnOn();
-            currentStep++;
-            if (currentStep >= correctSequence.Length)
-            {
-                resultText.text = "Succes!! PassWord = 1234";
-            }
-        }
-        else
-        {
-            RandomizeLights();
-            currentStep = 0;
-            //resultText.text = "잘못된 순서입니다. 랜덤으로 전구가 변경되었습니다.";
-        }
-    }
-
-    private void RandomizeLights()
-    {
-        foreach (LightController light in lights)
-        {
-            if (Random.value > 0.5f)
-            {
-                light.TurnOn();
-            }
-            else
-            {
-                light.TurnOff();
-            }
-        }
+        lightController.SwitchPressed(switchId);
     }
 }
